4a8d7deded5b730f1e70f69b678b9315a6030c9f
Felipe Rafael Wanner <felipe.w.pakua@gmail.com>
2021-03-07 10:32:25 -0300

    gramado image deleted, bug fixed

--- imagens.js
+++ imagens.js
@@ -1,13 +1,11 @@
 //imagem de fundo
 
-let gramado;
 let apple;
 let pear;
 let banana;
 let watermellon;
 
 function preload(){
-  gramado = loadImage("gramado.jpg");
   apple = loadImage("maca.png");
   pear = loadImage("pera.png");
   banana = loadImage("banana.png");
